a371d1e93e6cb2bc6c124aba4ddedbd6d42654e5
Umar Muneer <umarmuneer@gmail.com>
2021-06-13 18:49:25 +0500

    add default state value

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,16 @@ const go$ = timer(200, 500).pipe(
 const actions$ = merge(go$, resetClick$);
 const engine$ = startClick$.pipe(
   switchMapTo(actions$),
-  scan((state, action: any) => {
-    return action.type === "reset" ? 1 : state + 1;
-  }, 0)
+  scan(
+    (state: any, action: any) => {
+      if (action.type === "reset") {
+        return { ...action, value: 1 };
+      }
+      return { ...action, value: state.value + 1 };
+    },
+    { type: "none", value: 0 }
+  ),
+  map((action: any) => action.value)
 );
 engine$.subscribe((value: number) => {
   document.getElementById("numbers").textContent = value.toString();
