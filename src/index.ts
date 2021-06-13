419fa39857e34ad95dd49930e490931cfd882243
Umar Muneer <umarmuneer@gmail.com>
2021-06-13 18:31:36 +0500

    phase2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { fromEvent, timer, merge } from "rxjs";
-import { switchMapTo, takeUntil } from "rxjs/operators";
+import { scan, switchMapTo, takeUntil } from "rxjs/operators";
 const startClick$ = fromEvent(document.getElementById("start"), "click");
 const endClick$ = fromEvent(document.getElementById("end"), "click");
 const oneMinute$ = timer(60000);
 const end$ = merge(oneMinute$, endClick$)
-const go$ = timer(200, 1000).pipe(takeUntil(end$));
-const engine$ = startClick$.pipe(switchMapTo(go$));
+const go$ = timer(200, 500).pipe(takeUntil(end$));
+const engine$ = startClick$.pipe(switchMapTo(go$), scan(
+ (state, value: number) => {
+    return state + 1;
+ }, 0   
+));
 engine$.subscribe((value: number) => {
   document.getElementById("numbers").textContent = value.toString();
 });
