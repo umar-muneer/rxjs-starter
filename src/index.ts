d34a401a4ea7a6306e41c04a3a4c7dc8907f7ce2
Umar Muneer <umarmuneer@gmail.com>
2021-06-13 18:38:08 +0500

    phase3

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
 import { fromEvent, timer, merge } from "rxjs";
-import { scan, switchMapTo, takeUntil } from "rxjs/operators";
-const startClick$ = fromEvent(document.getElementById("start"), "click");
-const endClick$ = fromEvent(document.getElementById("end"), "click");
+import { map, scan, switchMapTo, takeUntil } from "rxjs/operators";
+const startButton = document.getElementById("start");
+const endButton = document.getElementById("end");
+const resetButton = document.getElementById("reset");
+const startClick$ = fromEvent(startButton, "click");
+const endClick$ = fromEvent(endButton, "click");
+const resetClick$ = fromEvent(resetButton, "click").pipe(
+  map(() => {
+    return {
+      type: "reset",
+      value: 0,
+    };
+  })
+);
 const oneMinute$ = timer(60000);
-const end$ = merge(oneMinute$, endClick$)
-const go$ = timer(200, 500).pipe(takeUntil(end$));
-const engine$ = startClick$.pipe(switchMapTo(go$), scan(
- (state, value: number) => {
-    return state + 1;
- }, 0   
-));
+const end$ = merge(oneMinute$, endClick$);
+const go$ = timer(200, 500).pipe(
+  map(() => {
+    return {
+      type: "data",
+    };
+  }),
+  takeUntil(end$)
+);
+const actions$ = merge(go$, resetClick$);
+const engine$ = startClick$.pipe(
+  switchMapTo(actions$),
+  scan((state, action: any) => {
+    return action.type === 'reset' ? 1 : state + 1;
+  }, 0)
+);
 engine$.subscribe((value: number) => {
   document.getElementById("numbers").textContent = value.toString();
 });
