8864b9bf9674df6c58a6a20b53aaa280485bf956
Umar Muneer <umarmuneer@gmail.com>
2021-06-13 18:54:08 +0500

    phase4

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { map, scan, switchMapTo, takeUntil } from "rxjs/operators";
 const startButton = document.getElementById("start");
 const endButton = document.getElementById("end");
 const resetButton = document.getElementById("reset");
+const multiplyBy2Button = document.getElementById("multiplyBy2");
 const startClick$ = fromEvent(startButton, "click");
 const endClick$ = fromEvent(endButton, "click");
 const resetClick$ = fromEvent(resetButton, "click").pipe(
@@ -10,6 +11,14 @@ const resetClick$ = fromEvent(resetButton, "click").pipe(
     return {
       type: "reset",
       value: 0,
+    };
+  })
+);
+const multiplyBy2Click$ = fromEvent(multiplyBy2Button, "click").pipe(
+  map(() => {
+    return {
+      type: "x2",
+      transform: (number) => number * 2,
     };
   })
 );
@@ -23,17 +32,17 @@ const go$ = timer(200, 500).pipe(
   }),
   takeUntil(end$)
 );
-const actions$ = merge(go$, resetClick$);
+const actions$ = merge(go$, resetClick$, multiplyBy2Click$);
 const engine$ = startClick$.pipe(
   switchMapTo(actions$),
   scan(
     (state: any, action: any) => {
       if (action.type === "reset") {
-        return { ...action, value: 1 };
+        return { ...state, ...action, value: 1 };
       }
-      return { ...action, value: state.value + 1 };
+      return { ...state, ...action, value: state.transform(state.value + 1) };
     },
-    { type: "none", value: 0 }
+    { type: "none", value: 0, transform: (number) => number }
   ),
   map((action: any) => action.value)
 );
