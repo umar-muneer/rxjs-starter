57bee8501bdda5220e9e3042402ce6beab09a1f5
Umar Muneer <umarmuneer@gmail.com>
2021-06-23 08:52:27 +0500

    use map instead of swtichMap to generate values

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import { fromEvent, timer } from "rxjs";
-import { switchMapTo, takeUntil } from "rxjs/operators";
+import { fromEvent, Observable, timer } from "rxjs";
+import { concatAll, map, takeUntil } from "rxjs/operators";
 const startClick$ = fromEvent(document.getElementById("start"), "click");
-const endClick$ = fromEvent(document.getElementById("end"), "click");
-const go$ = timer(200, 1000).pipe(takeUntil(endClick$));
-const engine$ = startClick$.pipe(switchMapTo(go$));
-engine$.subscribe((value: number) => {
-  document.getElementById("numbers").textContent = value.toString();
+const go$ = timer(200, 3000);
+const engine$ = startClick$.pipe(
+  map(() => {
+    return go$;
+  }),
+)
+/**
+ * problems
+ * we are getting an observable in the subscribe block instead of a value
+ * on each click we are generating a new observable which is creating multiple observables (we need to cancel the previous one)
+ * we need to have an internal map to 
+ */
+engine$.subscribe((value: Observable<number>) => {
+  value.subscribe((number) => {
+    document.getElementById("numbers").textContent = number.toString();
+  });
 });
