493fa3f87c11d92ea99efb024f9a7fbbf90832af
Umar Muneer <umarmuneer@gmail.com>
2021-06-23 09:08:24 +0500

    use takeUntil to cancel previous observables

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { fromEvent, Observable, timer } from "rxjs";
-import { concatAll, map, take, takeUntil } from "rxjs/operators";
+import { fromEvent, timer } from "rxjs";
+import { concatAll, map, takeUntil } from "rxjs/operators";
 const startClick$ = fromEvent(document.getElementById("start"), "click");
 const go$ = timer(200, 1000);
 const engine$ = startClick$.pipe(
   map(() => {
-    return go$
+    return go$.pipe(takeUntil(startClick$))
   }),
   concatAll()
 )
