43b304171b2e3641ecfbbcc5285d8823d503e5dd
Umar Muneer <umarmuneer@gmail.com>
2021-06-24 20:58:17 +0500

    gcam remove end click feature

--- src-old/index.ts
+++ src-old/index.ts
@@ -1,7 +1,7 @@
 const startButton = document.getElementById("start");
 const endButton = document.getElementById("end");
-let interval = null;
 startButton.addEventListener("click", () => {
+  let interval = null;
   let i = 0;
   if (interval) {
     clearInterval(interval);
@@ -10,10 +10,4 @@ startButton.addEventListener("click", () => {
     i += 1;
     document.getElementById("numbers").textContent = i.toString();
   }, 1000);
-});
-endButton.addEventListener("click", () => {
-  if (interval) {
-    clearInterval(interval);
-    interval = null;
-  }
 });
