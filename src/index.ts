c4f14ccc9fbc5f7aa1a10387404817315e18e913
Umar Muneer <umarmuneer@gmail.com>
2021-06-24 23:26:04 +0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { fromEvent, timer } from "rxjs";
 import { concatAll, map, takeUntil } from "rxjs/operators";
 const startClick$ = fromEvent(document.getElementById("start"), "click");
-const go$ = timer(200, 1000);
+const go$ = timer(200, 1000).pipe(takeUntil(startClick$));
 const engine$ = startClick$.pipe(
   map(() => {
-    return go$.pipe(takeUntil(startClick$))
+    return go$;
   }),
   concatAll()
-)
+);
 
 engine$.subscribe((value) => {
   document.getElementById("numbers").textContent = value.toString();
