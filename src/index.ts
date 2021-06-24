a0fe84e7bfffc6a4105df85fdf67df6b76ded08f
Umar Muneer <umarmuneer@gmail.com>
2021-06-24 23:27:45 +0500

    add spaces

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { fromEvent, timer } from "rxjs";
 import { switchMapTo, takeUntil } from "rxjs/operators";
 const startClick$ = fromEvent(document.getElementById("start"), "click");
 const endClick$ = fromEvent(document.getElementById("end"), "click");
+
 const go$ = timer(200, 1000).pipe(takeUntil(endClick$));
 const engine$ = startClick$.pipe(switchMapTo(go$));
+
 engine$.subscribe((value: number) => {
   document.getElementById("numbers").textContent = value.toString();
 });
